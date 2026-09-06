d0ecb8529eb2c62d55f3142a9ee7b61e307877ef
Ilnur Sultanov <ilnur.sultanow@gmail.com>
2026-09-05 21:03:52 -0300

    feat(Panel): added new panel symbols instead of old ones

--- src/Panel.cpp
+++ src/Panel.cpp
@@ -6,29 +6,29 @@
     
     void Panel::draw() {
             moveCursor(startPos.x, startPos.y);
-            std::cout << "+";
+            std::cout << "╔";
 
             for (int x = startPos.x + 1; x < endPos.x; x++)
-                std::cout << "-";
+                std::cout << "═";
 
-            std::cout << "+";
+            std::cout << "╗";
 
             for (int y = startPos.y + 1; y < endPos.y; y++)
             {
                 moveCursor(startPos.x, y);
-                std::cout << "|";
+                std::cout << "║";
 
                 for (int x = startPos.x + 1; x < endPos.x; x++)
                     std::cout << " ";
 
-                std::cout << "|";
+                std::cout << "║";
             }
 
             moveCursor(startPos.x, endPos.y);
-            std::cout << "+";
+            std::cout << "╚";
 
             for (int x = startPos.x + 1; x < endPos.x; x++)
-                std::cout << "-";
+                std::cout << "═";
 
-            std::cout << "+";
+            std::cout << "╝";
         }
