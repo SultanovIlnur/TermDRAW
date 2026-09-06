d5175ec40169e10a9c8b6b42fbea8648da22b7c4
Ilnur Sultanov <ilnur.sultanow@gmail.com>
2026-09-06 19:48:41 -0300

    feat(Canvas): added new methods for drawing ui elements

--- src/Canvas.cpp
+++ src/Canvas.cpp
@@ -6,3 +6,25 @@ Canvas::Canvas() : cursorPos{10,5} {} // default cursor position
 void Canvas::addShape(std::unique_ptr<Shape> shape) {
     shapes.push_back(std::move(shape));
 }
+
+void Canvas::clear() {
+    shapes.clear();
+}
+
+void Canvas::draw() const {
+    for (const auto& shape : shapes) {
+        shape->draw();
+    }
+
+    moveCursor(cursorPos.x, cursorPos.y);
+    std::cout << "\033[7m \033[27m"; // cursor of symbol represented as a reversed color square
+}
+
+Position2D Canvas::getCursor() const {
+    return cursorPos;
+}
+
+void Canvas::moveCursorBy(int dx, int dy) {
+    cursorPos.x += dx;
+    cursorPos.y += dy;
+}
